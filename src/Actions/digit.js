2e626ff61b1e2e83254ac3fe2b009b28b918715d
Adelon Zeta <adelonzeta@gmail.com>
2018-12-26 14:42:01 -0800

    fix zero issue in operandOne

--- src/Actions/digit.js
+++ src/Actions/digit.js
@@ -1,28 +1,31 @@
 export default (number, state) => {
-  let numberString = String(number)
-  let numberOne = state.operandOne
-  let numberTwo = state.operandTwo
+	let numberString = String(number)
+	let numberOne = state.operandOne
+	let numberTwo = state.operandTwo
 
-  if (state.operator) {
-    if (state.operandTwo) {
-      numberTwo =
-        state.operandTwo === "0"
-          ? numberString
-          : state.operandTwo + numberString
-    } else {
-      numberTwo = numberString
-    }
-  } else {
-    if (state.operandOne) {
-      numberOne = state.operandOne + numberString
-    } else {
-      numberOne = number === 0 ? null : numberString
-    }
-  }
+	if (state.operator) {
+		if (state.operandTwo) {
+			numberTwo =
+				state.operandTwo === '0'
+					? numberString
+					: state.operandTwo + numberString
+		} else {
+			numberTwo = numberString
+		}
+	} else {
+		if (state.operandOne) {
+			numberOne =
+				state.operandOne === '0'
+					? numberString
+					: state.operandOne + numberString
+		} else {
+			numberOne = numberString
+		}
+	}
 
-  return {
-    operandOne: numberOne,
-    operandTwo: numberTwo,
-    operator: state.operator
-  }
+	return {
+		operandOne: numberOne,
+		operandTwo: numberTwo,
+		operator: state.operator
+	}
 }
